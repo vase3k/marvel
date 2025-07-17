bcd2cf70283f1b6be03853a4e800d8740743eaf5
Vasiliy Vinogradov <vase3k@gmail.com>
2025-07-17 19:08:47 +0300

    random character

--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -1,6 +1,7 @@
 class MarvelService {
     _apiBase = 'https://marvel-server-zeta.vercel.app/';
     _apiKey = 'apikey=d4eecb0c66dedbfae4eab45d312fc1df';
+    _baseOffset = 210;
 
     getRessource = async url => {
         let res = await fetch(url);
@@ -13,11 +14,27 @@ class MarvelService {
     };
 
     getAllCharacters = async () => {
-        return this.getRessource(`${this._apiBase}characters?&${this._apiKey}`);
+        const res = await this.getRessource(`${this._apiBase}characters?&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     };
 
     getCharacter = async id => {
-        return this.getRessource(`${this._apiBase}characters?${id}?&${this._apiKey}`);
+        const res = await this.getRessource(`${this._apiBase}characters?&${this._apiKey}`).then(
+            res => res.data.results.filter(item => item.id === id)
+        );
+        return this._transformCharacter(res[0]);
+    };
+
+    _transformCharacter = char => {
+        return {
+            name: char.name,
+            description: char.description
+                ? `${char.description.slice(0, 210)}...`
+                : 'there is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        };
     };
 }
 
