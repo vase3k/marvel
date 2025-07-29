10dabfdcf487388004ffc1c8fa9c5b3fbc5aed75
Vasiliy Vinogradov <vase3k@gmail.com>
2025-07-29 21:14:47 +0300

    react router 5.3

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,51 +1,24 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { MainPage, ComicsPage } from '../pages';
 import AppHeader from '../appHeader/AppHeader';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-import ComicsList from '../../components/comicsList/ComicsList';
-import SingleComic from '../../components/singleComic/SingleComic';
-import AppBanner from '../../components/appBanner/AppBanner';
-
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    const [selectedChar, setSelectedChar] = useState(null),
-        [selectedComic, setSelectedComic] = useState(null);
-
-    const onCharSelected = id => {
-        setSelectedChar(id);
-    };
-
-    const onComicSelected = id => {
-        setSelectedComic(id);
-    };
-
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                <ErrorBoundary>
-                    <RandomChar />
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" />
-            </main>
-            <>
+        <Router>
+            <div className="app">
                 <AppHeader />
-                <AppBanner />
-                <ComicsList onComicSelected={onComicSelected} />
-                <SingleComic comicId={selectedComic} />
-            </>
-        </div>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage />
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     );
 };
 
